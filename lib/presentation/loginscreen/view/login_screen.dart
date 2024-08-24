0bd8afc76e05cff8c2556ef74d041e8bd15656a9
nasi321 <nasinasri6102@gmail.com>
2024-08-24 21:05:50 +0530

    modified

--- lib/presentation/loginscreen/view/login_screen.dart
+++ lib/presentation/loginscreen/view/login_screen.dart
@@ -13,106 +13,146 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(),
-      backgroundColor: ColorTheme.secondarycolor,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: size.height * 0.1),
-            SizedBox(height: size.height * 0.2),
-            Padding(
-              padding: EdgeInsets.only(left: size.width * .1, right: size.width * .1),
-              child: TextFormField(
-                controller: phoneController,
-                textInputAction: TextInputAction.next,
-                validator: (name) {
-                  if (name!.isEmpty) {
-                    return "Enter a valid phone number";
-                  } else {
-                    return null;
-                  }
-                },
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.phone,
-                  ),
-                  filled: true,
-                  hintText: 'Phone Number',
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(7),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/bgpic.png',
+              height: double.infinity,
+              fit: BoxFit.cover,
             ),
-            SizedBox(height: size.height * 0.07),
-            InkWell(
-              onTap: () async {
-                try {
-                  await FirebaseAuth.instance.verifyPhoneNumber(
-                    phoneNumber: phoneController.text,
-                    verificationCompleted: (phoneAuthCredential) {
-                      // Auto verification succeeded, sign in directly
-                      FirebaseAuth.instance.signInWithCredential(phoneAuthCredential);
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeScreen()),
-                            (route) => false,
-                      );
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                // top: size.height * 0.2,
+                left: size.width * .1,
+                right: size.width * .1,
+                bottom: size.height * 0.6,
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: size.height * 0.1),
+                  SizedBox(height: size.height * 0.2),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: size.width * .01, right: size.width * .01),
+                    child: TextFormField(
+                      controller: phoneController,
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.phone,
+                      validator: (number) {
+                        if (number!.isEmpty) {
+                          return "Enter a valid phone number";
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.phone,
+                          color: ColorTheme.maincolor,
+                        ),
+                        filled: false,
+                        hintText: 'Phone Number ',
+                        hintStyle: TextStyle(color: ColorTheme.maincolor),
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(7),
+                            borderSide: BorderSide(
+                                width: 5, color: ColorTheme.maincolor)),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          borderSide:
+                              BorderSide(color: ColorTheme.maincolor, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          borderSide:
+                              BorderSide(color: ColorTheme.maincolor, width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.05),
+                  InkWell(
+                    onTap: () async {
+                      try {
+                        await FirebaseAuth.instance.verifyPhoneNumber(
+                          phoneNumber: phoneController.text,
+                          verificationCompleted: (phoneAuthCredential) {
+                            // Auto verification succeeded, sign in directly
+                            FirebaseAuth.instance
+                                .signInWithCredential(phoneAuthCredential);
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeScreen()),
+                              (route) => false,
+                            );
+                          },
+                          verificationFailed: (error) {
+                            log('Verification failed: ${error.message}');
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Verification failed')),
+                            );
+                          },
+                          codeSent: (verificationId, forceResendingToken) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    OtpScreen(verificationId: verificationId),
+                              ),
+                            );
+                          },
+                          codeAutoRetrievalTimeout: (verificationId) {
+                            log("Auto retrieval timeout");
+                          },
+                        );
+                      } catch (e) {
+                        log('Error during phone number verification: $e');
+                      }
                     },
-                    verificationFailed: (error) {
-                      log('Verification failed: ${error.message}');
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Verification failed')),
-                      );
-                    },
-                    codeSent: (verificationId, forceResendingToken) {
+                    child: Container(
+                      height: size.height * 0.05,
+                      width: size.width * 0.57,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            color: ColorTheme.maincolor,
+                            width: 2,
+                          ),
+                          color: ColorTheme.maincolor,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: ColorTheme.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.3),
+                  TextButton(
+                    onPressed: () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => OtpScreen(verificationId: verificationId),
+                          builder: (context) => RegistrationScreen(),
                         ),
                       );
                     },
-                    codeAutoRetrievalTimeout: (verificationId) {
-                      log("Auto retrieval timeout");
-                    },
-                  );
-                } catch (e) {
-                  log('Error during phone number verification: $e');
-                }
-              },
-              child: Container(
-                height: size.height * 0.06,
-                width: size.width * 0.4,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: Text(
-                    "LOGIN",
+                    child: Text("Don't have an account? Register",
+                        style: TextStyle(
+                            color: ColorTheme.maincolor,
+                            fontWeight: FontWeight.bold)),
                   ),
-                ),
+                ],
               ),
             ),
-            SizedBox(height: size.height * 0.3),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RegistrationScreen(),
-                  ),
-                );
-              },
-              child: Text(
-                "Don't have an account? Register",
-
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/presentation/signup_screen/view/signup_screen.dart
+++ lib/presentation/signup_screen/view/signup_screen.dart
@@ -1,155 +1,201 @@
 import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:whitematrix_mt/core/constants/colors.dart';
 import '../../loginscreen/view/login_screen.dart';
 import '../../otp_screen/view/otp_screen.dart';
 
 class RegistrationScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final phoneNumberController = TextEditingController();
     final nameController = TextEditingController();
 
-
     var size = MediaQuery.sizeOf(context);
     return Scaffold(
-
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: size.height * 0.2,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: ColorTheme.secondarycolor,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/bgpic.png',
+              height: double.infinity,
+              fit: BoxFit.cover,
             ),
-            Text(
-              "WELCOME TO BAG NEST",
-
-            ),
-            SizedBox(
-              height: size.height * 0.06,
-            ),
-            Padding(
-              padding:
-              EdgeInsets.only(left: size.width * .1, right: size.width * .1),
-              child: TextFormField(
-                controller: nameController,
-                textInputAction: TextInputAction.next,
-                validator: (name) {
-                  if (name!.isEmpty) {
-                    return "Enter a valid Username";
-                  } else {
-                    return null;
-                  }
-                },
-
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.person,
-
-                  ),
-                  filled: true,
-
-                  hintText: 'Name',
-
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide: BorderSide.none),
-                ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                // top: size.height * 0.2,
+                left: size.width * .1,
+                right: size.width * .1,
+                bottom: size.height * 0.6,
               ),
-            ),
-            SizedBox(
-              height: size.height * 0.05,
-            ),
-            Padding(
-              padding:
-              EdgeInsets.only(left: size.width * .1, right: size.width * .1),
-              child: TextFormField(
-                controller: phoneNumberController,
-                textInputAction: TextInputAction.next,
-                // validator: (phonenumber) {
-                //   if (phonenumber!.isEmpty) {
-                //     return "Enter a valid phone number";
-                //   } else {
-                //     return null;
-                //   }
-                // },
-
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.person,
-
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: size.height * 0.2,
                   ),
-                  filled: true,
-
-                  hintText: 'Phone Number',
-
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide: BorderSide.none),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: size.height * 0.05,
-            ),
-            InkWell(
-              onTap: () async {
-                try {
-                  await FirebaseAuth.instance.verifyPhoneNumber(
-                    phoneNumber: phoneNumberController.text,
-                    verificationCompleted: (phoneAuthCredential) {},
-                    verificationFailed: (error) {
-                      log('Verification failed: ${error.message}');
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Verification failed')),
-                      );
-                    },
-                    codeSent: (verificationId, forceResendingToken) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => OtpScreen(verificationId: verificationId),
+                  SizedBox(
+                    height: size.height * 0.06,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: size.width * .01, right: size.width * .01),
+                    child: TextFormField(
+                      controller: nameController,
+                      textInputAction: TextInputAction.next,
+                      validator: (name) {
+                        if (name!.isEmpty) {
+                          return "Enter a valid Username";
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.person_2_outlined,
+                          color: ColorTheme.maincolor,
                         ),
-                      );
-                    },
-                    codeAutoRetrievalTimeout: (verificationId) {
-                      log("Auto retrieval timeout");
-                    },
-                  );
-                } catch (e) {
-                  log('Error during phone number verification: $e');
-                }
-              },
-              child: Container(
-                height: size.height * 0.06,
-                width: size.width * 0.4,
-                decoration: BoxDecoration(
-
-                    borderRadius: BorderRadius.circular(10)),
-                child: Center(
-                  child: Text(
-                    "SEND",
-
+                        filled: false,
+                        hintText: 'Enter Your Name ',
+                        hintStyle: TextStyle(color: ColorTheme.maincolor),
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          borderSide: BorderSide(color: ColorTheme.maincolor),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          borderSide: BorderSide(
+                              color: ColorTheme.maincolor,
+                              width:
+                                  2), // Set border color to black for enabled state
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          borderSide:
+                              BorderSide(color: ColorTheme.maincolor, width: 2),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: size.width * .01, right: size.width * .01),
+                    child: TextFormField(
+                      controller: phoneNumberController,
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.phone,
+                      // validator: (phonenumber) {
+                      //   if (phonenumber!.isEmpty) {
+                      //     return "Enter a valid phone number";
+                      //   } else {
+                      //     return null;
+                      //   }
+                      // },
+
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.phone,
+                          color: ColorTheme.maincolor,
+                        ),
+                        filled: false,
+                        hintText: 'Phone Number ',
+                        hintStyle: TextStyle(color: ColorTheme.maincolor),
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(7),
+                            borderSide: BorderSide(
+                                width: 5, color: ColorTheme.maincolor)),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          borderSide:
+                              BorderSide(color: ColorTheme.maincolor, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          borderSide:
+                              BorderSide(color: ColorTheme.maincolor, width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      try {
+                        await FirebaseAuth.instance.verifyPhoneNumber(
+                          phoneNumber: phoneNumberController.text,
+                          verificationCompleted: (phoneAuthCredential) {},
+                          verificationFailed: (error) {
+                            log('Verification failed: ${error.message}');
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Verification failed')),
+                            );
+                          },
+                          codeSent: (verificationId, forceResendingToken) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    OtpScreen(verificationId: verificationId),
+                              ),
+                            );
+                          },
+                          codeAutoRetrievalTimeout: (verificationId) {
+                            log("Auto retrieval timeout");
+                          },
+                        );
+                      } catch (e) {
+                        log('Error during phone number verification: $e');
+                      }
+                    },
+                    child: Container(
+                      height: size.height * 0.05,
+                      width: size.width * 0.57,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            color: ColorTheme.maincolor,
+                            width: 2,
+                          ),
+                          color: ColorTheme.maincolor,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                        child: Text(
+                          "SEND",
+                          style: TextStyle(color: ColorTheme.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.3,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginScreen()));
+                      },
+                      child: Text(
+                        "Already have an account? Login",
+                        style: TextStyle(
+                            color: ColorTheme.maincolor,
+                            fontWeight: FontWeight.bold),
+                      ))
+                ],
               ),
             ),
-            SizedBox(
-              height: size.height * 0.3,
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()));
-                },
-                child: Text(
-                  "Already have an account? Login",
-
-                ))
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
